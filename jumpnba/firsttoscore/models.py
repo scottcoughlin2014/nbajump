ee4f405e94ecced99327a0b2725d042e78534474
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2021-01-01 13:42:26 -0600

    Some more updates

--- jumpnba/firsttoscore/models.py
+++ jumpnba/firsttoscore/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from players.models import Player
-from datetime import date
-
 
 # Create your models here.
 class FanDuelOdds(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     first_to_score_odds = models.FloatField()
     date = models.DateTimeField()
+
+class FirstScorer(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    number_of_times = models.IntegerField(null=True, blank=True)
--- jumpnba/jumpnba/settings.py
+++ jumpnba/jumpnba/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'players',
     'firsttoscore',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
