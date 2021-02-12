4c4308763e9aaf9b3b28777b140875aff090c840
Giacomo Terreran <giacomoterreran@Giacomos-MacBook-Pro.local>
2021-02-12 17:13:13 -0600

    s

--- jumpnba/jumpnba/settings.py
+++ jumpnba/jumpnba/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'players',
+    'teams',
     'firsttoscore',
+    'games',
     'django_extensions',
 ]
 
