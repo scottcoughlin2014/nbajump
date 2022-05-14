c53973ef44c634fa519c4fe3d6a9482a1bbd681b
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2022-05-14 10:26:43 -0500

    Making it work in Docker

--- jumpnba/settings.py
+++ jumpnba/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'jumpnba.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
